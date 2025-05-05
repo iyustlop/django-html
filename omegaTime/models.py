4ca8e3945e5a712e2ab8abdc73bad7c1814c44c7
iyustlop <Log1t3ch>
2025-05-05 23:33:33 +0200

    Define model draft

--- omegaTime/models.py
+++ omegaTime/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Worker(models.Model):
+  name = models.CharField(max_length=200)
+
+class Account(models.Model):
+  account = models.CharField(max_length=100)
+  worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
