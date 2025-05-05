6b2be19521116b205e1cd45ef37c54f87f76e13f
iyustlop <Log1t3ch>
2025-05-05 23:07:59 +0200

    urls moves to app

--- omegaOn/urls.py
+++ omegaOn/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from omegaTime import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
-    path('about/', views.about)
+    path('', include('omegaTime.urls'))
 ]
