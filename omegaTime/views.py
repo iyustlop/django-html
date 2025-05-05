c2a2a053bfbe19b6caaf1eb5af0ef48fcecb8191
iyustlop <Log1t3ch>
2025-05-06 00:12:40 +0200

    draft index.html

--- omegaTime/views.py
+++ omegaTime/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Index Page")
+  return render(request, 'index.html')
 
 def worker(request):
   workers = list(Worker.objects.values())
